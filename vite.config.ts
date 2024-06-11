dc8ff6c14242e431f68592f59af548e37ce9c728
Fanil Borkhanov <Fanil.Borkhanov@vmedia.ca>
2024-06-11 12:32:24 +0300

    added base

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
       additionalLegacyPolyfills: ['regenerator-runtime/runtime']
     })
   ],
+  base: '/tg-vue/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
