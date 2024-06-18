4d62a1e1d5f657bafcc15d6a3ade3803bff42728
Fanil Borkhanov <Fanil.Borkhanov@vmedia.ca>
2024-06-18 14:03:31 +0300

    added axios

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,10 +7,28 @@
 <script lang="ts">
 import Vue from "vue";
 import TheWelcome from '@/components/TheWelcome.vue';
+import axios from 'axios';
+interface Data {
+    name: string;
+    email: string;
+    referralCode: string;
+    responseMessage: string;
+    user: any;
+}
 export default Vue.extend({
   name: 'HomeView',
   components: {
       TheWelcome,
+    },
+
+     data(): Data {
+        return {
+          name: 'test',
+          email: 'test email',
+          referralCode: '',
+          responseMessage: '',
+          user: null
+        };
     },
 
   mounted() {
@@ -23,6 +41,21 @@ export default Vue.extend({
             Telegram.WebApp.showAlert('Не смотри еще не готово')
         });	
         Telegram.WebApp.MainButton.show();
+        this.user = Telegram.WebApp.initDataUnsafe
+        this.registerUser();
+  },
+  methods: {
+    async registerUser() {
+      try {
+        const response = await axios.post('http://127.0.0.1:5000/register', {
+          username: this.user.user.username,
+          id: this.user.user.id,
+        });
+        this.responseMessage = `Your referral code: ${response.data.referral_code}`;
+      } catch (error) {
+        this.responseMessage = 'Error registering user: ' + error.response.data.error;
+      }
+    },
   },
   beforeDestroy() {
     Telegram.WebApp.MainButton.hide();
